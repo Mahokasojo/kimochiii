f89d73e08d9bd79bfa033d90a5183db72e2d3087
ShiroyukiHiro <raditya2587@gmail.com>
2015-06-17 10:32:18 +0700

    Revert "HashiHiro"

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "HashiHiro",
+  originalName = "NotReallyABot",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
